894bf757cb7a0bffd352aa3b6afd1fe29b9aba76
LeafDaun <117880860+LeafDaun@users.noreply.github.com>
2024-06-06 11:27:40 +0800

    Update ShowMahasiswa.php

--- app/Livewire/ShowMahasiswa.php
+++ app/Livewire/ShowMahasiswa.php
@@ -8,11 +8,21 @@ use App\Models\Mahasiswa;
 
 class ShowMahasiswa extends Component
 {
+    public $jurusanID;
+    public $mahasiswaID; 
+    
+    public function updatedjurusanID()
+    {
+        // return $this->jurusanID;
+    }
+
     public function render()
     {
         return view('livewire.show-mahasiswa', [
             'mahasiswa' => Mahasiswa::all(),
-            'jurusan' => Jurusan::findorfail(1)
+            'combo_jurusan' => Jurusan::all(),
+            'jurusan' => Jurusan::find($this->jurusanID),
+            
         ]);
     }
 }
