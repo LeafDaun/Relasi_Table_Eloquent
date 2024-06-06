40967b0ec9eb77cc846cdf28dedf45b742b88780
LeafDaun <117880860+LeafDaun@users.noreply.github.com>
2024-06-06 11:29:13 +0800

    Update show-mahasiswa.blade.php

--- resources/views/livewire/show-mahasiswa.blade.php
+++ resources/views/livewire/show-mahasiswa.blade.php
@@ -17,21 +17,35 @@
         @endforeach
     </table>
     <br>
-    <h5>Data Jurusan : {{ $jurusan->nama_jurusan }}</h5>
+
+         <div class="mb-3 float-start">
+                <select wire:model.live="jurusanID"  class="form-select">
+                    <option selected>-->pilih<--</option>
+
+                    @foreach ($combo_jurusan as $jurus)
+                        <option value="{{ $jurus->id }}">{{ $jurus->nama_jurusan }}</option>
+                    @endforeach
+        
+                </select>
+            </div>
+
     <table class="table table-striped">
         <tr>
             <th>No</th>
             <th>Nama</th>
             <th>Alamat</th>
+            <th>Jurusan</th>
             
         </tr>
-        @foreach ($jurusan->mahasiswa as $mhs)
-            <tr>
-                <td>{{ $loop->iteration }}</td>
-                <td>{{ $mhs->nama }}</td>
-                <td>{{ $mhs->alamat }}</td>
-                
-            </tr>
-        @endforeach
+        @isset($jurusan->mahasiswa)
+            @foreach ($jurusan->mahasiswa as $mhs)
+                <tr>
+                    <td>{{ $loop->iteration }}</td>
+                    <td>{{ $mhs->nama }}</td>
+                    <td>{{ $mhs->alamat }}</td>
+                    <td>{{ $mhs->jurusan->nama_jurusan }}</td>
+                </tr>
+            @endforeach
+        @endisset
     </table>
 </div>
